a8a6f994adc2d79c34dce6ce1dea3ea2a9ff7587
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-21 16:22:34 +0700

    Add files via upload

---
+++ lib/logics/parser/parser.h
@@ -0,0 +1,22 @@
+#include <stdio.h>   // deklarasi library yang akan digunakan
+#include <string.h>  // deklarasi library yang akan digunakan
+#include <stdlib.h>  // deklarasi library yang akan digunakan
+#include <ctype.h>  // deklarasi library yang akan digunakan
+#include "../lexer/lexer.h"
+
+void parserOperator(char *token, char* str);
+void parserStrcmp(char* left, char* op, char* right, char* result);
+void parserAuraOperation(char pita[]);
+void parseAssignment(char* line);
+void mainParser(char* line);
+void skibidi(char* line);
+void skibidiToilet(char* line);
+void sigma(char* line);
+void sigmaBoy(char* line);
+void gyatt(char* line);
+void bombaclat(char* line);
+void tungtungtung(char* line);
+void lingaguliguli(char* line);
+void goofy(char* line);
+void sus(char* line);
+void ahh(char* line);
